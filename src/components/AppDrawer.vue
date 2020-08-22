c58b9472a906aca3ce0f690f27f00eeb8f82ee2b
farshiana <alizee.farshian@gmail.com>
2020-08-22 16:12:03 +0200

    Add app drawer admin

--- src/components/AppDrawer.vue
+++ src/components/AppDrawer.vue
@@ -1,5 +1,5 @@
 <template>
-    <v-navigation-drawer v-if="isAuthenticated" app bottom :mobile-breakpoint="960">
+    <v-navigation-drawer v-if="isAuthenticated" app>
         <v-list-item>
             <v-list-item-content>
                 <v-list-item-title class="title">
