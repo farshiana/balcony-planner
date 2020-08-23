271c7ac5b3f4b3b6a16158fa4262f5e3e8204d3b
farshiana <alizee.farshian@gmail.com>
2020-08-23 09:31:39 +0200

    Add constants

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
+// CATEGORIES
 export const CATEGORY_FRUITS = 'fruits';
 export const CATEGORY_HERBS = 'herbs';
 export const CATEGORY_VEGETABLES = 'vegetables';
@@ -5,4 +6,24 @@ export const CATEGORIES = [
     CATEGORY_FRUITS,
     CATEGORY_HERBS,
     CATEGORY_VEGETABLES,
+];
+
+// EXPOSURES
+export const EXPOSURE_SHADE = 'shade';
+export const EXPOSURE_SUN = 'sun';
+export const EXPOSURE_PARTIAL = 'partial';
+export const EXPOSURES = [
+    EXPOSURE_SHADE,
+    EXPOSURE_SUN,
+    EXPOSURE_PARTIAL,
+];
+
+// WATERINGS
+export const WATERING_LOW = 'low';
+export const WATERING_MEDIUM = 'medium';
+export const WATERING_HIGH = 'high';
+export const WATERINGS = [
+    WATERING_LOW,
+    WATERING_MEDIUM,
+    WATERING_HIGH,
 ];
