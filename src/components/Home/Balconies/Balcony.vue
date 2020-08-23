b4cd7afa6b57caca8292ed6f87f3560b95749baf
farshiana <alizee.farshian@gmail.com>
2020-08-23 23:38:45 +0200

    Clean moveable

--- src/components/Home/Balconies/Balcony.vue
+++ src/components/Home/Balconies/Balcony.vue
@@ -1,45 +1,17 @@
 <template>
-    <Moveable v-bind="moveable">
-        <span>Vue Moveable</span>
-    </Moveable>
+    <div>toto</div>
 </template>
 
 <script>
 import { mapState, mapActions } from 'vuex';
-import Moveable from 'vue-moveable';
 
 export default {
-    components: {
-        Moveable,
-    },
     props: {
         balcony: {
             type: Object,
             required: true,
         },
     },
-    data: () => ({
-        moveable: {
-            draggable: true,
-            // snappable: true,
-            bounds: {
-                left: 40,
-                top: 40,
-                right: 600,
-                bottom: 430,
-            },
-            // throttleDrag: 0,
-            // resizable: false,
-            // throttleResize: 1,
-            // keepRatio: false,
-            // scalable: true,
-            // throttleScale: 0,
-            // rotatable: true,
-            // throttleRotate: 0,
-            // pinchable: true, // ["draggable", "resizable", "scalable", "rotatable"]
-            // origin: false,
-        },
-    }),
     created() {
         this.loadPlanters(this.balcony.id);
     },
@@ -48,11 +20,6 @@ export default {
     },
     methods: {
         ...mapActions('planters', ['loadPlanters']),
-
-        // onDrag({ target, transform }) {
-        //     console.log('onDrag', transform);
-        //     target.style.transform = transform;
-        // },
     },
 };
 </script>
